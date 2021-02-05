ea814a75cfdd4ad80c5c0d34749bb0a723023439
Mahmoud Bakr <mbakr6821@gmail.com>
2021-02-05 11:32:07 +0200

    setup linters

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="header"><small>This is just initial.</small> Hold on!</h1>
+      <h1 className="header">
+        <small>This is just initial.</small>
+        {' '}
+        Hold on!
+      </h1>
       <code>deployed on Heroku!</code>
     </div>
   );
