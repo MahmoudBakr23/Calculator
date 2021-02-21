116c93e66644453884462230180d0e194314ae7c
Mahmoud Bakr <mbakr6821@gmail.com>
2021-02-21 15:24:57 +0200

    change the default App SFC to class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="header">
-        <small>Milestone 2</small>
-      </h1>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="header">
+          <small>Milestone 2</small>
+        </h1>
+      </div>
+    );
+  }
 }
-
-export default App;
