2bc31f3b233ef118862996cfa7d53d5f2a273b16
Mahmoud Bakr <mbakr6821@gmail.com>
2021-02-22 00:37:56 +0200

    add react.fragment

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import ButtonPanel from './Components/ButtonPanel';
 export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="header">Milestone 2</h1>
-        <div className="Calculator">
-          <Display result="0" />
-          <ButtonPanel />
+      <>
+        <div className="App">
+          <h1 className="header">Milestone 2</h1>
+          <div className="Calculator">
+            <Display result="0" />
+            <ButtonPanel />
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
