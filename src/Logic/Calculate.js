7a11a5bbe627802943d42fee3df26221a4b64ca9
Mahmoud Bakr <mbakr6821@gmail.com>
2021-02-27 17:43:40 +0200

    create = button

--- src/Logic/Calculate.js
+++ src/Logic/Calculate.js
@@ -4,4 +4,28 @@ export default function calculate(data, buttonName) {
   let {
     total, next, operation, answer,
   } = data;
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const operators = ['+', 'x', '-', '÷'];
+
+  if (buttonName === '=') {
+    if (total && next && operation) {
+      total = operate(total, next, operation);
+      answer = total;
+      next = null;
+      operation = null;
+      total = null;
+    } else if (answer && next && operation) {
+      total = operate(answer, next, operation);
+      answer = total;
+      next = null;
+      operation = null;
+      total = null;
+    } else if (total && answer && operation) {
+      total = operate(total, answer, operation);
+      answer = total;
+      next = null;
+      operation = null;
+      total = null;
+    }
+  }
 }
