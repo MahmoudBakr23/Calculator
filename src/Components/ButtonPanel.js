6520fca15b3297093dcf952f6fc4ab3e8e0c4162
Mahmoud Bakr <mbakr6821@gmail.com>
2021-02-27 17:50:39 +0200

    create and validate property clickHandler for ButtonPanel Component

--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -1,38 +1,44 @@
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = () => (
+const ButtonPanel = ({ clickHandler }) => (
   <div className="ButtonPanel">
     <div className="buttons group1">
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" />
-      <Button name="AC" className="ac" />
+      <Button name="+/-" handler={clickHandler} />
+      <Button name="%" handler={clickHandler} />
+      <Button name="÷" handler={clickHandler} />
+      <Button name="AC" className="ac" handler={clickHandler} />
     </div>
     <div className="buttons group2">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="×" />
+      <Button name="7" handler={clickHandler} />
+      <Button name="8" handler={clickHandler} />
+      <Button name="9" handler={clickHandler} />
+      <Button name="x" handler={clickHandler} />
     </div>
     <div className="buttons group3">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
-      <Button name="-" />
+      <Button name="4" handler={clickHandler} />
+      <Button name="5" handler={clickHandler} />
+      <Button name="6" handler={clickHandler} />
+      <Button name="-" handler={clickHandler} />
     </div>
     <div className="buttons group4">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
-      <Button name="+" />
+      <Button name="1" handler={clickHandler} />
+      <Button name="2" handler={clickHandler} />
+      <Button name="3" handler={clickHandler} />
+      <Button name="+" handler={clickHandler} />
     </div>
     <div className="buttons group5">
-      <Button name="del" className="del" />
-      <Button name="0" />
-      <Button name="." />
-      <Button name="=" className="eq" />
+      <Button name="del" className="del" handler={clickHandler} />
+      <Button name="0" handler={clickHandler} />
+      <Button name="." handler={clickHandler} />
+      <Button name="=" className="eq" handler={clickHandler} />
+      <Button name="A" className="answer" handler={clickHandler} />
     </div>
   </div>
 );
+
+Button.propTypes = {
+  clickHandler: PropTypes.isRequired,
+};
 
 export default ButtonPanel;
